5e7f8c2d1dadce085dc26950a9000f336ab8800c
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-28 13:45:05 -0500

    wip

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <img className="logo" src="../logocropped-768x238.png" alt="Bradens logo"></img>
+        
+      </div>
+
+    )
+  }
+}
+
+
+
+
+export default App;
